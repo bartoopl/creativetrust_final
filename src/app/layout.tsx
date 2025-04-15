c362ecbdb37f09c4f4384b89d91820cac603dd9b
Bartosz <bartosz.lysniewski@gmail.com>
2025-04-15 21:00:53 +0200

    Update Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     return (
         <html lang="pl" className="overflow-x-hidden">
         <head>
-            {/* Google Tag Manager - umieść przed zamknięciem tagu head */}
+            {/* Google Tag Manager i GA4 - poprawiona implementacja */}
             <script
                 dangerouslySetInnerHTML={{
                     __html: `
@@ -55,9 +55,73 @@ export default function RootLayout({
                             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                             })(window,document,'script','dataLayer','GTM-WTKHM3K');
+                            
+                            // Google Analytics 4 bezpośrednia implementacja (pomocnicza)
+                            (function(w,d,s,l,i){
+                                w[l]=w[l]||[];w[l].push({'gtm.start':
+                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                                j=d.createElement(s);j.async=true;j.src=
+                                'https://www.googletagmanager.com/gtag/js?id='+i;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','gtag','G-2PK8VH2GDV');
+                            
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-2PK8VH2GDV');
                         `,
                 }}
             />
+
+            {/* Sprawdzanie wcześniejszych zgód */}
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        // Sprawdź zapisane zgody przy ładowaniu strony
+                        (function() {
+                            var storedConsent = localStorage.getItem('cookieConsent');
+                            if (storedConsent) {
+                                window.dataLayer = window.dataLayer || [];
+                                
+                                if (storedConsent === 'all') {
+                                    window.dataLayer.push({
+                                        event: 'consent_update',
+                                        consent: {
+                                            ad_storage: 'granted',
+                                            analytics_storage: 'granted',
+                                            functionality_storage: 'granted',
+                                            personalization_storage: 'granted',
+                                            security_storage: 'granted'
+                                        }
+                                    });
+                                    console.log('Consent restored from storage: all granted');
+                                    
+                                    // Dodatkowe potwierdzenie dla GA4
+                                    if (typeof gtag === 'function') {
+                                        gtag('consent', 'update', {
+                                            ad_storage: 'granted',
+                                            analytics_storage: 'granted'
+                                        });
+                                        console.log('GA4 consent updated directly');
+                                    }
+                                } else if (storedConsent === 'essential') {
+                                    window.dataLayer.push({
+                                        event: 'consent_update',
+                                        consent: {
+                                            ad_storage: 'denied',
+                                            analytics_storage: 'denied',
+                                            functionality_storage: 'granted',
+                                            personalization_storage: 'denied',
+                                            security_storage: 'granted'
+                                        }
+                                    });
+                                    console.log('Consent restored from storage: only essential granted');
+                                }
+                            }
+                        })();
+                    `,
+                }}
+            />
+            <title>Creative Trust</title>
         </head>
         <body className={`${manrope.className} overflow-x-hidden w-full`}>
         {/* Google Tag Manager (noscript) - umieść na początku body */}
